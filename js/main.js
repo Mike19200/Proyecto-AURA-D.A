8b210e48f8e7aac2cbdb703a0998a090c4c5056c
Mike <83485035+Mike19200@users.noreply.github.com>
2023-08-02 09:15:57 -0500

    animacion botones

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const startPage = document.getElementById("startPage");
 const introductionPage = document.getElementById("introductionPage");
 
 startbtn.addEventListener("click", function() {
-  startPage.style.display = "none";
-  introductionPage.style.display = "block";
+  // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
+  if (window.innerWidth <= 768) {
+    // Esperar 2 segundos antes de cambiar la sección
+    setTimeout(function() {
+      startPage.style.display = "none";
+      introductionPage.style.display = "block";
+    }, 800);
+  } else {
+    // Cambiar la sección inmediatamente en otros dispositivos
+    startPage.style.display = "none";
+    introductionPage.style.display = "block";
+  }
 });
