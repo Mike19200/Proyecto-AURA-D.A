cb490706a93df5faf9bcf3125553ba3ea3afec1a
Mike <83485035+Mike19200@users.noreply.github.com>
2023-08-02 14:26:56 -0500

    Creacion de namePage y birthdatePage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const startbtn = document.getElementById("startbtn");
+const continuebtn1 = document.getElementById("continuebtn1");
+const continuebtn2 = document.getElementById("continuebtn2");
+
 const startPage = document.getElementById("startPage");
 const introductionPage = document.getElementById("introductionPage");
+const namePage = document.getElementById("namePage");
+const birthdatePage = document.getElementById("birthdatePage");
 
 startbtn.addEventListener("click", function() {
   // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
@@ -14,5 +19,41 @@ startbtn.addEventListener("click", function() {
     // Cambiar la sección inmediatamente en otros dispositivos
     startPage.style.display = "none";
     introductionPage.style.display = "block";
+  }
+});
+
+continuebtn1.addEventListener("click", function() {
+  // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
+  if (window.innerWidth <= 768) {
+    // Esperar 2 segundos antes de cambiar la sección
+    setTimeout(function() {
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "block";
+    }, 800);
+  } else {
+    // Cambiar la sección inmediatamente en otros dispositivos
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "block";
+  }
+});
+
+continuebtn2.addEventListener("click", function() {
+  // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
+  if (window.innerWidth <= 768) {
+    // Esperar 2 segundos antes de cambiar la sección
+    setTimeout(function() {
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "none";
+      birthdatePage.style.display = "block";
+    }, 800);
+  } else {
+    // Cambiar la sección inmediatamente en otros dispositivos
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "none";
+      birthdatePage.style.display = "block";
   }
 });
