af637261a5486e512979d990beb6eeffcaa093d3
Mike <83485035+Mike19200@users.noreply.github.com>
2023-08-02 23:18:54 -0500

    generating aura page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const startbtn = document.getElementById("startbtn");
 const continuebtn1 = document.getElementById("continuebtn1");
 const continuebtn2 = document.getElementById("continuebtn2");
+const continuebtn3 = document.getElementById("continuebtn3");
 
 const startPage = document.getElementById("startPage");
 const introductionPage = document.getElementById("introductionPage");
 const namePage = document.getElementById("namePage");
 const birthdatePage = document.getElementById("birthdatePage");
+const generatingPage = document.getElementById("generatingPage");
 
 startbtn.addEventListener("click", function() {
   // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
@@ -23,16 +25,13 @@ startbtn.addEventListener("click", function() {
 });
 
 continuebtn1.addEventListener("click", function() {
-  // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
   if (window.innerWidth <= 768) {
-    // Esperar 2 segundos antes de cambiar la sección
     setTimeout(function() {
       startPage.style.display = "none";
       introductionPage.style.display = "none";
       namePage.style.display = "block";
     }, 800);
   } else {
-    // Cambiar la sección inmediatamente en otros dispositivos
       startPage.style.display = "none";
       introductionPage.style.display = "none";
       namePage.style.display = "block";
@@ -40,9 +39,7 @@ continuebtn1.addEventListener("click", function() {
 });
 
 continuebtn2.addEventListener("click", function() {
-  // Verificar si la página está abierta en un dispositivo móvil (ancho menor a 768px)
   if (window.innerWidth <= 768) {
-    // Esperar 2 segundos antes de cambiar la sección
     setTimeout(function() {
       startPage.style.display = "none";
       introductionPage.style.display = "none";
@@ -50,10 +47,27 @@ continuebtn2.addEventListener("click", function() {
       birthdatePage.style.display = "block";
     }, 800);
   } else {
-    // Cambiar la sección inmediatamente en otros dispositivos
       startPage.style.display = "none";
       introductionPage.style.display = "none";
       namePage.style.display = "none";
       birthdatePage.style.display = "block";
+  }
+});
+
+continuebtn3.addEventListener("click", function() {
+  if (window.innerWidth <= 768) {
+    setTimeout(function() {
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "none";
+      birthdatePage.style.display = "none";
+      generatingPage.style.display = "block";
+    }, 800);
+  } else {
+      startPage.style.display = "none";
+      introductionPage.style.display = "none";
+      namePage.style.display = "none";
+      birthdatePage.style.display = "none";
+      generatingPage.style.display = "block";
   }
 });
